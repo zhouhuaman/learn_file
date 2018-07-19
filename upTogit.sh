aac559d349f37d7aa697d307a6e8f57bd385d83c
zhouhuaman <zhouhuaman@163.com>
2018-07-19 21:27:12 +0800

    update commit

--- upTogit.sh
+++ upTogit.sh
@@ -5,6 +5,6 @@ git status #查看当前工作区的状态（需提交的变更）
 git config --global user.email "zhouhuaman@163.com"
 git config --global user.name "zhouhuaman"
 git commit -m "update commit" #把文件提交到仓库
-git remote rm origin http://github.com/zhouhuaman/learn_file.git 
 git remote add origin http://github.com/zhouhuaman/learn_file.git
+git pull origin master
 git push origin master #将本地主分支推到远程主分支
